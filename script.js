c4d8eab15ec748b7e297f8fd2424add8725c95c1
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-04-10 20:37:43 +0200

    changing color animation

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const wordLetters = document.querySelectorAll('.tile');
 const buttons = document.querySelectorAll('.key');
 const WORDS =['kamil', 'karol', 'konik', 'kutas'];
 let isGuessed = new Array(WORD_LENGTH).fill(0);
-const animation_duration = 500; // ms
+const animation_duration = 500;
 
 function generateWord(){
     word = WORDS[Math.floor(Math.random()*WORDS.length)];
@@ -19,7 +19,6 @@ function addLetter(val) {
     if (inputWord.length < WORD_LENGTH) {
         inputWord += val;
         wordLetters[attempts * WORD_LENGTH + inputWord.length - 1].innerHTML = val.toUpperCase();
-        console.log(inputWord === word);
     }
 }
 function backspace() {
@@ -52,11 +51,14 @@ function colorButton(char, color) {
 function resetWord(){
     attempts = 0;
     generateWord();
+
     wordLetters.forEach((letter) =>{
         letter.style.backgroundColor = "#131314";
         letter.style.color = '#fff';
         letter.innerHTML = "";
+        letter.classList.remove("animated")
     })
+
     buttons.forEach(element => {
         element.style.backgroundColor = '#818384';
         element.style.color = '#fff';
@@ -73,60 +75,88 @@ function submitWord() {
     const letterMatches = [];
 
     for(let i = 0; i < WORD_LENGTH; i++){
+
         let letter = inputWord[i];
-        wordLetters[WORD_LENGTH * attempts + i].innerHTML = letter.toUpperCase();
+        let element = wordLetters[WORD_LENGTH * attempts + i];
+        element.innerHTML = letter.toUpperCase();
+
         if(word.includes(letter)){
             if (word[i] === letter) {
-                wordLetters[WORD_LENGTH * attempts + i].classList.add('animated');
-                wordLetters[WORD_LENGTH * attempts + i].style.backgroundColor = '#4CAF50';
-                wordLetters[WORD_LENGTH * attempts + i].style.color = '#fff';
-                wordLetters[WORD_LENGTH * attempts + i].style.animationDelay = `${(i * animation_duration) / 2}ms`;
                 isGuessed[i] = 1;
-                colorButton(letter, 1);
             } else {
                 letterMatches.push([letter,i]);
             }
         }
-        else{
-            wordLetters[WORD_LENGTH * attempts + i].classList.add('animated');
-            wordLetters[WORD_LENGTH * attempts + i].style.animationDelay = `${(i * animation_duration) / 2}ms`;
-
-            colorButton(letter, 0);
-        }
     }
-    console.log(letterMatches);
-    console.log(word);
 
     for (let i = 0; i < letterMatches.length; i++){
         let letter = letterMatches[i][0];
-        let letter_idx = letterMatches[i][1];
         let idx = word.indexOf(letter);
+
         while (idx !== -1) {
             if (isGuessed[idx] === 0){
                 isGuessed[idx] = 2;
-                wordLetters[WORD_LENGTH * attempts + letter_idx].classList.add('animated');
-                wordLetters[WORD_LENGTH * attempts + letter_idx].style.backgroundColor = '#ffcc00'
-                wordLetters[WORD_LENGTH * attempts + letter_idx].style.color = '#000';
-                wordLetters[WORD_LENGTH * attempts + i].style.animationDelay = `${(i * animation_duration) / 2}ms`;
-
-                colorButton(letter, 2);
                 break;
             }
             idx = word.indexOf(letter, idx + 1);
         }
     }
+
+    // painting board and keyboard
+    for (let i = 0; i < WORD_LENGTH; i++){
+        let element = wordLetters[WORD_LENGTH * attempts + i];
+        let letter = element.textContent.toLowerCase();
+
+        switch (isGuessed[i]) {
+            case 0:
+                setTimeout(() =>{
+                    colorButton(letter, 0);
+                    element.style.backgroundColor = '#333334';
+                    element.style.color = '#fff';
+                }, (i * animation_duration) + 700 / 2);
+                element.classList.add('animated');
+                element.style.animationDelay = `${i * animation_duration}ms`;
+                break;
+            case 1:
+                setTimeout(() =>{
+                    colorButton(letter, 1);
+                    element.style.backgroundColor = '#4CAF50';
+                    element.style.color = '#fff';
+                }, (i * animation_duration) + 700 / 2);
+                element.classList.add('animated');
+
+                element.style.animationDelay = `${i * animation_duration}ms`;
+                break;
+            case 2:
+                setTimeout(() =>{
+                    colorButton(letter, 2);
+                    element.style.backgroundColor = '#ffcc00';
+                    element.style.color = '#000';
+                }, (i * animation_duration) + 700 / 2);
+                element.classList.add('animated');
+
+                element.style.animationDelay = `${i * animation_duration}ms`;
+                break;
+        }
+    }
+
     attempts += 1;
-    isGuessed.fill(0);
+    console.log(word === inputWord);
 
     if (word === inputWord){
-        alert(`You won in ${attempts} attempts`);
-        resetWord();
+        setTimeout(() => {
+            console.log(word === inputWord);
+            alert(`You won in ${attempts} attempts`);
+            resetWord();
+        }, 6 * animation_duration);
     }
     else if (attempts === MAX_ATTEMPTS){
-        alert(`You lost! To many attempts!\nThe word was ${word}`);
-        resetWord();
+        setTimeout(() => {
+            alert(`You lost! To many attempts!\nThe word was ${word}`);
+            resetWord();
+        }, 6 * animation_duration);
     }
     inputWord = '';
-
+    isGuessed.fill(0);
 }
 generateWord();
