699a7e1686fe7b70ed93a961dc9ec06b6de28039
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-11-22 13:06:05 +0100

    pop up instruction window

--- script.js
+++ script.js
@@ -175,3 +175,17 @@ function submitWord() {
     tile_color.fill(0);
 }
 generateWord();
+
+// POP-UP WINDOW JS
+
+var popupLink = document.getElementById("popup-link");
+var popupWindow = document.getElementById("popup-window");
+var closeButton = document.getElementById("close-button");
+popupLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    popupWindow.style.display = "block";
+});
+
+closeButton.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+});
